641b39751b207e2ad37c4ad8ffcb2a60bb8ae488
Thom Hunkin <thom.hunkin@gmail.com>
2024-12-08 10:45:25 +0000

    Finished

--- filter.js
+++ filter.js
@@ -21,6 +21,11 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(p => p.name === "Paul");
+console.log(paul);
 
 // Complex Filtering
 const students = [
@@ -55,3 +60,10 @@ const students = [
     ]
   },
 ];
+
+const has5YearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5YearsExp).length > 0;
+const candidates = students.filter(hasStrongSkills);
+const names = candidates.map(na => na.name);
+console.log(candidates);
+console.log(names);
